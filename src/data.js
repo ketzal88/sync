5352108f7c227d3b172e6909159b0de4617610af
Matías Giménez <meganiunmati@hotmail.com>
2022-04-27 12:21:10 -0300

    add ui apartments page

--- src/data.js
+++ src/data.js
@@ -1,30 +1,97 @@
-let apartments = [
+const apartments = [{
+  "id": "UF305",
+  "superficie": "50m2",
+  "piso": 1,
+  "dormitorios": 3,
+  "amenities": ["laundry", "garage"],
+  "pdf": "https:linkpdf",
+  "responsableLink": "http: link a calendly",
+  "responsableNombre": "Pirulo",
+  "img1": "https://images.adsttc.com/media/images/5ca6/130c/284d/d1a3/1e00/02fb/large_jpg/2018.03.03_Starship_Landing_Luna_Plan.jpg?1554387716",
+  "tipo": "monoambiente"
+  },
+  {
+  "id": "UF306",
+  "superficie": "50m2",
+  "piso": 1,
+  "dormitorios": 3,
+  "amenities": ["laundry", "garage"],
+  "pdf": "https:linkpdf",
+  "responsableLink": "http: link a calendly",
+  "responsableNombre": "Pirulo",
+  "img1": "https://images.adsttc.com/media/images/5ca6/130c/284d/d1a3/1e00/02fb/large_jpg/2018.03.03_Starship_Landing_Luna_Plan.jpg?1554387716",
+  "tipo": "dosambientes"
+  },
+  {
+  "id": "UF307",
+  "superficie": "50m2",
+  "piso": 1,
+  "dormitorios": 3,
+  "amenities": ["laundry", "garage"],
+  "pdf": "https:linkpdf",
+  "responsableLink": "http: link a calendly",
+  "responsableNombre": "Pirulo",
+  "img1": "https://images.adsttc.com/media/images/5ca6/130c/284d/d1a3/1e00/02fb/large_jpg/2018.03.03_Starship_Landing_Luna_Plan.jpg?1554387716",
+  "tipo": "tresambientes"
+  }
+  ]
+/* let apartments = [
     {
-      name: "Santa Monica",
-      price: "$10,800",
+      imagen: 'https://lumen.uv.mx/recursoseducativos/PlanosArquitectonicos/img/escritorio/conceptos/planosinstalaciones4.png',
+      name: "UF 305",
+      sizes: '65.43',
+      unit: 'M2',
+      characteristics: [
+        '2do piso',
+        '2 dormitorios'
+      ],
       id: 0,
     },
     {
-      name: "Stankonia",
-      price: "$8,000",
+      imagen: 'https://lumen.uv.mx/recursoseducativos/PlanosArquitectonicos/img/escritorio/conceptos/planosinstalaciones4.png',
+      name: "UF 305",
+      sizes: '65.43',
+      unit: 'M2',
+      characteristics: [
+        '2do piso',
+        '2 dormitorios'
+      ],
       id: 1,
     },
     {
-      name: "Ocean Avenue",
-      price: "$9,500",
+      imagen: 'https://lumen.uv.mx/recursoseducativos/PlanosArquitectonicos/img/escritorio/conceptos/planosinstalaciones4.png',
+      name: "UF 305",
+      sizes: '65.43',
+      unit: 'M2',
+      characteristics: [
+        '2do piso',
+        '2 dormitorios'
+      ],
       id: 2,
     },
     {
-      name: "Tubthumper",
-      price: "$14,000",
+      imagen: 'https://lumen.uv.mx/recursoseducativos/PlanosArquitectonicos/img/escritorio/conceptos/planosinstalaciones4.png',
+      name: "UF 305",
+      sizes: '65.43',
+      unit: 'M2',
+      characteristics: [
+        '2do piso',
+        '2 dormitorios'
+      ],
       id: 3,
     },
     {
-      name: "Wide Open Spaces",
-      price: "$4,600",
+      imagen: 'https://lumen.uv.mx/recursoseducativos/PlanosArquitectonicos/img/escritorio/conceptos/planosinstalaciones4.png',
+      name: "UF 305",
+      sizes: '65.43',
+      unit: 'M2',
+      characteristics: [
+        '2do piso',
+        '2 dormitorios'
+      ],
       id: 4,
     },
-  ];
+  ]; */
   
   export function getApartments() {
     return apartments;
--- src/routes/apartmentsCatalogue.jsx
+++ src/routes/apartmentsCatalogue.jsx
@@ -1,33 +1,10 @@
-import { NavLink, Outlet } from "react-router-dom";
-import { getApartments } from "../data";
+import React, { Fragment } from "react";
+import ApartmentsPage from "../components/ApartmentsPage/ApartmentsPage";
 
 export default function ApartmentsCatalogue() {
-    let apartments = getApartments();
-    return (
-			<div style={{ display: "flex" }}>
-        <nav
-          style={{
-            borderRight: "solid 1px",
-            padding: "1rem",
-          }}
-        >
-          {apartments.map((apartment) => (
-            <NavLink
-              style={({ isActive }) => {
-								return {
-									display: "block",
-									margin: "1rem 0",
-									color: isActive ? "red" : "",
-								};
-							}}
-              to={`/apartments/${apartment.id}`}
-              key={apartment.id}
-            >
-              {apartment.name}
-            </NavLink>
-          ))}
-        </nav>
-				<Outlet />
-      </div>
-    );
-  }
+  return (
+    <Fragment>
+      <ApartmentsPage />
+    </Fragment>
+  );
+}
