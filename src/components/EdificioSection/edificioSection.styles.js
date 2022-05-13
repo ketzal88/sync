0d6e418f7a18b9b5f01b818ea047fec2e3a763c1
Marco Baiad <marcobaiad@gmail.com>
2022-05-12 22:16:39 -0300

    se agregan styles de 1366 en edificio section

--- src/components/EdificioSection/edificioSection.styles.js
+++ src/components/EdificioSection/edificioSection.styles.js
@@ -4,34 +4,54 @@ import edificio from "../../assets/images/edificio.png";
 export const useEdificioStyles = makeStyles((theme) => {
   return {
     container: {
+      backgroundImage: `url(${edificio})`,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      [theme.breakpoints.up("lg")]: {
+        backgroundPosition: "-350px 0px",
+        height: "769px",
+      },
       [theme.breakpoints.up("xl")]: {
-        backgroundImage: `url(${edificio})`,
-        backgroundSize: "cover",
         backgroundPosition: "-750px 0px",
-        backgroundRepeat: "no-repeat",
         height: "1149px",
       },
     },
     secondWrapper: {
+      [theme.breakpoints.up("lg")]: {
+        marginTop: "93px",
+        marginRight: "124px",
+      },
       [theme.breakpoints.up("xl")]: {
         marginTop: "80px",
         marginRight: "191px",
       },
     },
     titleSection: {
+      color: "black",
+      [theme.breakpoints.up("lg")]: {
+        fontSize: "48px !important",
+      },
       [theme.breakpoints.up("xl")]: {
-        color: "black",
         fontSize: "60px !important",
       },
     },
     listLabel: {
+      color: "#384146",
+      [theme.breakpoints.up("lg")]: {
+        fontSize: "20px !important",
+        marginBottom: "35px !important",
+      },
       [theme.breakpoints.up("xl")]: {
         fontSize: "28px !important",
         marginBottom: "49px !important",
-        color: "#384146",
       },
     },
     listQuantity: {
+      fontWeight: "bold !important",
+      [theme.breakpoints.up("lg")]: {
+        fontSize: "34px !important",
+        lineHeight: "1.2 !important",
+      },
       [theme.breakpoints.up("xl")]: {
         fontSize: "60px !important",
         fontWeight: "bold !important",
